766006f99562d430cd208b89a4664ea2ee71d8e2
scarpino <scarpino@utexas.edu>
2019-10-07 15:35:45 -0400

    intial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+## Code and data for influenza in the United States (1998- Sept. 2019)
+
+### Citation
+N/A
+
+### Notes on the code
+The .R files contain the code to build the data files and some summary figures.  In line comments mention some key gaps. 
+
+### Data
+1. ILINet.csv - US CDC FluView https://gis.cdc.gov/grasp/fluview/fluportaldashboard.html. Downloaded on Sept. 24th 2019.
+2. WHO_NREVSS_Clinical_Labs.csv - US CDC FluView https://gis.cdc.gov/grasp/fluview/fluportaldashboard.html. Downloaded on Sept. 24th 2019.
+3. WHO_NREVSS_Combined_prior_to_2015_16.csv - US CDC FluView https://gis.cdc.gov/grasp/fluview/fluportaldashboard.html. Downloaded on Sept. 24th 2019.
+4. WHO_NREVSS_Public_Health_Labs.csv - US CDC FluView https://gis.cdc.gov/grasp/fluview/fluportaldashboard.html. Downloaded on Sept. 24th 2019.
+5. us_pops.csv - Data were copy/pasted from https://www.multpl.com/united-states-population/table/by-year on Sept. 24th 2019.  They ballpark look right, but we should circle back and use US census data.
+
+### CDC Disclaimer
+This was copy/pasted from https://gis.cdc.gov/grasp/fluview/fluportaldashboard.html on Oct 7th 2019
+
+#### Disclaimer
+Viral Surveillance — Data collection from both the U.S. World Health Organization (WHO) Collaborating Laboratories and National Respiratory and Enteric Virus Surveillance System (NREVSS) laboratories began during the 1997-98 season. The volume of tested specimens has greatly increased during this time due to increased participation and increased testing. During the 1997-98 season 43 state public health laboratories participated in surveillance, and by the 2004-05 season all state public health laboratories were participating in surveillance. The addition of NREVSS data during the 1997-98 season roughly doubled the amount of virologic data reported each week.
+
+The number of specimens tested and % positive rate vary by region and season based on different testing practices including triaging of specimens by the reporting labs, therefore it is not appropriate to compare the magnitude of positivity rates or the number of positive specimens between regions or seasons.
+
+The U.S. WHO and NREVSS collaborating laboratories report the total number of respiratory specimens tested and the number positive for influenza types A and B each week to CDC. Most of the U.S. WHO collaborating laboratories also report the influenza A subtype (H1 or H3) of the viruses they have isolated, but the majority of NREVSS laboratories do not report the influenza A subtype.
+
+For more information on virologic surveillance please visit:http://www.cdc.gov/flu/weekly/overview.htm#Viral
+
+Outpatient Illness Surveillance — Information on patient visits to health care providers for influenza-like illness is collected through the U.S. Outpatient Influenza-like Illness Surveillance Network (ILINet). This collaborative effort between CDC, state and local health departments, and health care providers started during the 1997-98 influenza season when approximately 250 providers were enrolled. Enrollment in the system has increased over time and there were >3,000 providers enrolled during the 2010-11 season.
+
+The number and percent of patients presenting with ILI each week will vary by region and season due to many factors, including having different provider type mixes (children present with higher rates of ILI than adults, and therefore regions with a higher percentage of pediatric practices will have higher numbers of cases). Therefore it is not appropriate to compare the magnitude of the percent of visits due to ILI between regions and seasons.
+
+Baseline levels are calculated both nationally and for each region. Percentages at or above the baseline level are considered to be elevated.
+
+For more information on ILI surveillance and baselines please visit:http://www.cdc.gov/flu/weekly/overview.htm#Outpatient
+
+### License
+(see LICENSE)
+
+## Additional license, warranty, and copyright information
+We provide a license for our code (see LICENSE) and do not claim ownership, nor the right to license, the data we have obtained.  Please cite the appropriate agency, paper, and/or individual in publications and/or derivatives using these data, contact them regarding the legal use of these data, and remember to pass-forward any existing license/warranty/copyright information.  As a reminder, THE DATA AND SOFTWARE ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE DATA AND/OR SOFTWARE OR THE USE OR OTHER DEALINGS IN THE DATA AND/OR SOFTWARE.
---
+++ US_influenza_data.R
@@ -0,0 +1,115 @@
+#SV Scarpino
+#National ILI, flu lab tests, and total influenza cases
+#Sept 2019
+
+###########
+#libraries#
+###########
+library(ggplot2)
+
+###########
+#acc funcs#
+###########
+
+
+###############
+#Global Params#
+###############
+save_results <- FALSE
+make_plots <- FALSE
+time_stamp <- as.numeric(Sys.time())
+
+######
+#data#
+######
+ili <- read.csv("Data/ILINet.csv")
+lab_old <- read.csv("Data/WHO_NREVSS_Combined_prior_to_2015_16.csv", skip = 1)
+lab_new <- read.csv("Data/WHO_NREVSS_Clinical_Labs.csv", skip = 1)
+pop <- read.csv("Data/us_pops.csv")
+pops <- pop$Value[match(ili$YEAR, pop$Date)]*1000000 #US pop is in millions 
+
+############
+#Build data#
+############
+#1. Multiply ili by percent pos. from WHO labs
+ili_raw_raw <- ili$X..WEIGHTED.ILI
+ili_raw <- ili_raw_raw/100
+lab_pos_raw <- c(lab_old$PERCENT.POSITIVE, lab_new$PERCENT.POSITIVE) #NOTE should add a code check here that the dates line up (checked manually on Oct. 7th 2019)
+lab_pos <- lab_pos_raw/100
+flu_ili_who_raw <- ili_raw * lab_pos
+flu_ili_who <- flu_ili_who_raw * pops
+flu_ili_who_corrected <- flu_ili_who * 0.7 #Rolfes et al. 2019, see ciz075.pdf in the lit folder, estimated that the 2017-2018 influenza season was ~70% lower than what one obtains from ili_pos * who_pos. NOTE - should return and use the actual values from Rolfes with confidence intervals
+
+#2. build season indicator
+season <- rep(NA, nrow(ili))
+start <- 1997:2018
+stop <- 1998:2019
+for(i in 1:length(start)){
+  sea.1.i <- which(ili$WEEK >= 40 & ili$YEAR == start[i])
+  sea.2.i <- which(ili$WEEK <= 20 & ili$YEAR == stop[i])
+  season[c(sea.1.i, sea.2.i)] <- paste0(start[i], "-", stop[i])
+}
+
+pan1 <- which(ili$WEEK >= 16 & ili$WEEK <= 33 & ili$YEAR == 2009) #note, should come back and check these against the literature
+season[pan1] <- "2009 Pandemic First Wave"
+
+pan2 <- which(ili$WEEK > 33 & ili$YEAR == 2009 | ili$WEEK <= 20 & ili$YEAR == 2010) #note, should come back and check these against the literature
+season[pan2] <- "2009 Pandemic Second Wave"
+
+#3. building data file
+out <- data.frame(season, ili$YEAR, ili$WEEK, flu_ili_who, flu_ili_who_corrected, ili$X..WEIGHTED.ILI, lab_pos, pops)
+colnames(out) <- c("Season", "Year", "Week", "Estimated_total_flu_US_ILI_WHO",  "Estimated_total_flu_US_ILI_WHO_RolfesCorrected", "ILI_per_pop_weighted", "Percent_Pos_Labs", "US_pop")
+
+if(save_results == TRUE){
+  #this is currently influenza_usa_sept2019.csv in the Results directory
+  filename <- paste0(time_stamp, "influenza_usa.csv")
+  write.csv(out, file = filename)
+}
+
+#4. Aggregate metrics
+max_sea <- by(out$Estimated_total_flu_US_ILI_WHO_RolfesCorrected, out$Season, max, na.rm = T)
+tot_sea <- by(out$Estimated_total_flu_US_ILI_WHO_RolfesCorrected, out$Season, sum, na.rm = T)
+
+pop_out <- c(pop$Value[23:12], pop$Value[11], pop$Value[11], pop$Value[10:2])*1000000 #this is obviously bad and needs to be fixed
+out2 <- data.frame(names(max_sea), as.numeric(max_sea), as.numeric(tot_sea), pop_out, as.numeric(tot_sea)/pop_out)
+colnames(out2) <- c("season", "peak_cases", "total_cases", "population", "total_prop")
+
+if(save_results == TRUE){
+  #this is currently flu_season_summary_USA_sept2019.csv in the Results directory
+  filename_season <- paste0(time_stamp, "flu_season_summary_usa.csv")
+  write.csv(out2, file = filename_season, row.names = FALSE, quote = FALSE)
+}
+
+#######
+#Plots#
+#######
+if(make_plots == TRUE){
+  #I. Time series
+  #quartz()
+  y <- out$Estimated_total_flu_US_ILI_WHO_RolfesCorrected
+  x <- 1:length(y) #should use package to transform CDC weeks in dates.
+  
+  plot(x, y, type = "l", main = "Influenza in United States 1998 - 2019", xlab = " ", ylab = "Estimated influenza cases", xaxt = "n", col = "#000000", lty = 1, lwd = 3, bty = "n")
+  at.x <- (1:nrow(out))[which(out[,"Week"] == 1)]
+  lab.x <- out[,"Year"][which(out[,"Week"] == 1)]
+  axis(1, at = at.x, labels = lab.x)
+  
+  #II. Peak cases by season
+  #quartz()
+  par(mar = c(15, 4, 4, 2))
+  bplt <- barplot(out2$peak_cases, xaxt = "n", ylab = "Peak number of influenza cases", main = "Peak influenza cases in the United States")
+  labs <- as.character(out2$season)
+  text(bplt, par("usr")[3]-0.25, labels = labs, srt = 60, adj = c(1.1,1.1), xpd = TRUE, cex = 1) #this isn't quite right in terms of the label position, note the pandemics are too low.
+  
+  #III. Total prop infected by season
+  #quartz()
+  par(mar = c(15, 4, 4, 2))
+  bplt2 <- barplot(out2$total_prop, xaxt = "n", ylab = "Total proportion of US infected with influenza", main = "Total proportion infected with influenza in the United States")
+  labs <- as.character(out2$season)
+  text(bplt2, 0, labels = labs, srt = 60, adj = c(1.1,1.1), xpd = TRUE, cex = 1) #this isn't quite right in terms of the label position, note the pandemics are too low.
+  
+  #IV. Total cases by season
+  #quartz()
+  hist(out2$total_cases, col = "gray", xlab = "Total US influenza cases", main = "Total number of influenza cases in the US by season")
+  abline(v = mean(out2$total_cases), col = "red", lwd = 3, lty = 3)
+}
